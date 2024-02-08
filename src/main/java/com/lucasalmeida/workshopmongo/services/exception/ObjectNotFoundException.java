e18313ba4f80a655dab5a7ae97b4665475fbff84
Lucas Almeida <dev2@case.inf.br>
2024-02-07 23:35:22 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/lucasalmeida/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.lucasalmeida.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super();
+	}
+}
